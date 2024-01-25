8d4a51f4a16bc2d4fc007f64956a137a217a99ba
AlessandroBilli <skillmanbungo@gmail.com>
2024-01-25 19:53:41 +0100

    fix: folder

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+
+<body>
+    <h1> Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa ea, porro placeat voluptatum eos tempore deleniti provident odio. Nihil, quam vitae itaque doloremque aliquid consectetur commodi necessitatibus voluptatibus rerum. Placeat.</p>
+</body>
+
+</html>
